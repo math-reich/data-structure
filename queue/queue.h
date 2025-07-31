98627600fbe6def541f343ffe9bab934d8ac2e07
math_Reich <108031464+math-Reich@users.noreply.github.com>
2025-07-31 17:17:16 -0300

    start queue

--- queue/queue.h
+++ queue/queue.h
@@ -5,6 +5,15 @@
 
 typedef struct {
    int* data;
-} dynamic_queue;
+   int rear;
+   int front;
+   int capacity;
+} circular_queue;
+
+circular_queue* array_queue_create(int capacity);
+int array_queue_destroy(circular_queue* queue);
+int array_queue_enqueue(int data);
+int array_queue_dequeue();
+
 
 #endif
